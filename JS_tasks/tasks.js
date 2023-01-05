90d13b665d3ea3abd152d1291568bd879277f1ea
liza-rgb <77597961+hshshshhshsshshsh@users.noreply.github.com>
2023-01-05 18:40:48 +0100

    Implemented js task #1

---
+++ JS_tasks/tasks.js
@@ -0,0 +1,9 @@
+// TASK1
+
+const filter_list = (list) => {
+  return list.filter((el) => typeof el === "number");
+};
+
+console.log(filter_list([1, 2, "a", "b"]));
+console.log(filter_list([1, "a", "b", 0, 15]));
+console.log(filter_list([1, 2, "aasf", "1", "123", 123]));
