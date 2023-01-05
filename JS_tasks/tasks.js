2828878a6384addcb34ad1fbe8f61b80d31d6b78
liza-rgb <77597961+hshshshhshsshshsh@users.noreply.github.com>
2023-01-05 18:42:46 +0100

    Added js task #2

--- JS_tasks/tasks.js
+++ JS_tasks/tasks.js
@@ -7,3 +7,22 @@ const filter_list = (list) => {
 console.log(filter_list([1, 2, "a", "b"]));
 console.log(filter_list([1, "a", "b", 0, 15]));
 console.log(filter_list([1, 2, "aasf", "1", "123", 123]));
+
+// TASK2
+
+const first_non_repeating_letter = (input) => {
+  input = input.replaceAll(" ", "");
+  for (let i = 0; i < input.length; i++) {
+    const regex = new RegExp(
+      `${input[i].toLowerCase()}|${input[i].toUpperCase()}`,
+      "g"
+    );
+    const modified_input = input.replaceAll(regex, "");
+    if (modified_input.length === input.length - 1) {
+      return input[i];
+    }
+  }
+  return "";
+};
+console.log(first_non_repeating_letter("stress"));
+console.log(first_non_repeating_letter("sTreSS"));
